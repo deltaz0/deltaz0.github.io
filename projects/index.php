c72542d7d1f81f655ea1b4a5c96b5e5612fce891
Sam Johnson <diztech@live.com>
2017-11-10 18:20:32 -0600

    Update and rename index.php to index.html

--- projects/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-set_include_path( get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] );
-include_once('header.php');
-?>
-<p>My Other Projects</p>
-<?php projectsmenu() ?>
-<?php
-include_once('footer.php');
-?>
